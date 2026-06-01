0f9e07372964e85121cc5d5b8bb07f5289ea49ec
gulshanky1 <117905842+gulshanky1@users.noreply.github.com>
2026-06-01 21:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// your code here
+ document.getElementById("button").addEventListener("click", () => {
+
+            const name = document.getElementById("name").value.trim();
+            const year = document.getElementById("year").value.trim();
+
+            let url = "https://localhost:8080/";
+            let params = [];
+
+            if (name) {
+                params.push(`name=${encodeURIComponent(name)}`);
+            }
+
+            if (year) {
+                params.push(`year=${year}`);
+            }
+
+            if (params.length > 0) {
+                url += "?" + params.join("&");
+            }
+
+            document.getElementById("url").textContent = url;
+        });
